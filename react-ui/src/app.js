d43dd17a1538ef6845fac9dd03a82862514b5de1
Stephen O'Halloran <sohalloran@salesforce.com>
2018-05-17 04:55:34 +0100

    added canvas

--- react-ui/src/app.js
+++ react-ui/src/app.js
@@ -23,8 +23,7 @@ class App extends Component {
   }
   updateCanvas() {
       const context = this.refs.canvas.getContext('2d');
-      var img=document.getElementById("my-image");
-      context.drawImage(img,10,10);  
+      context.drawImage({file && file.preview},500,500);  
       context.beginPath();
       context.rect(188, 50, 200, 100);
       context.fillStyle = 'yellow';
