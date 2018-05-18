8f77048e0a26ae9c3d02e9077e2e76a715b2044c
Stephen O'Halloran <sohalloran@salesforce.com>
2018-05-18 18:41:22 +0100

    reset

--- react-ui/src/predictions.js
+++ react-ui/src/predictions.js
@@ -40,21 +40,21 @@ class Predictions extends Component {
         <
         div className = "predictions" > {
           interpolatingStyles.map((style, i) => {
-              const prediction = contents[i];
-              if (prediction == null) {
-                return null;
-              }
-              const probability = prediction.probability;
-              const percent = Math.round(probability * 100);
-              const labels = prediction.label.split(/,\s*/);
-              const boundingBox = prediction.boundingBox;
-              const minX = boundingBox.minX;
-              const minY = boundingBox.minY;
-              const maxX = boundingBox.maxX;
-              const maxY = boundingBox.maxY;
-              let color = '#fff';
-              if (probability < .5) color = '#777';
-              return ( < div onClick = {
+            const prediction = contents[i];
+            if (prediction == null) {
+              return null;
+            }
+            const probability = prediction.probability;
+            const percent = Math.round(probability * 100);
+            const labels = prediction.label.split(/,\s*/);
+            const boundingBox = prediction.boundingBox;
+            const minX = boundingBox.minX;
+            const minY = boundingBox.minY;
+            const maxX = boundingBox.maxX;
+            const maxY = boundingBox.maxY;
+            let color = '#fff';
+            if (probability < .5) color = '#777';
+            return ( < div onClick = {
                   () => this.hightlight(minX, minY, maxX, maxY)
                 }
                 className = 'prediction'
@@ -81,26 +81,26 @@ class Predictions extends Component {
                 }) < span className = "probability"
                 title = "Probability" > {
                   percent
-                } % < /span></h
-                2 > {
-                  labels[1] != null ?
-                  <
-                  p className = "alt-labels" > {
-                    labels.slice(1, labels.length).join(', ')
-                  } < /p> :
-                  null
-                }
-
+                } % < /span> <
+                /h2> {
+                labels[1] != null ?
                 <
-                /div>);
-              })
-          } <
-          /div>
+                p className = "alt-labels" > {
+                  labels.slice(1, labels.length).join(', ')
+                } < /p> :
+                null
+              }
+
+              <
+              /div>);
+          })
         } <
-        /StaggeredMotion>
+        /div>
+      } <
+      /StaggeredMotion>
 
-      );
-    }
+    );
   }
+}
 
-  export default Predictions;
+export default Predictions;
