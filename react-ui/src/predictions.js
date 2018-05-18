406239c4cb06fb2006153b6341a94da1d2279f27
Stephen O'Halloran <sohalloran@salesforce.com>
2018-05-18 18:37:29 +0100

    reset

--- react-ui/src/predictions.js
+++ react-ui/src/predictions.js
@@ -9,98 +9,98 @@ import {
 
 class Predictions extends Component {
   static propTypes = {
-    // The Predictive Vision Predictions
-    contents: React.PropTypes.array.isRequired
-  }
-  highlight: function (minX, minY, maxX, maxY) = {
-    console.log(minX, minY, maxX, maxY);
-  }
-  render() {
-    const contents = this.props.contents;
-    if (contents == null || contents.length === 0) {
-      return <div className = "empty" / > ;
-    }
-
-    return ( < StaggeredMotion defaultStyles = {
-        contents.map(p => ({
-          maxHeight: 0
-        }))
+      // The Predictive Vision Predictions
+      contents: React.PropTypes.array.isRequired
+    },
+    hightlight: function (value) {
+      console.log(value);
+    },
+    render() {
+      const contents = this.props.contents;
+      if (contents == null || contents.length === 0) {
+        return <div className = "empty" / > ;
       }
-      styles = {
-        prevInterpolatedStyles =>
-        prevInterpolatedStyles.map((_, i) =>
-          i === 0 ? {
-            maxHeight: spring(100)
-          } : {
-            maxHeight: spring(prevInterpolatedStyles[i - 1].maxHeight)
-          }
-        )
-      } > {
-        interpolatingStyles =>
-        <
-        div className = "predictions" > {
-          interpolatingStyles.map((style, i) => {
-              const prediction = contents[i];
-              if (prediction == null) {
-                return null;
-              }
-              const probability = prediction.probability;
-              const percent = Math.round(probability * 100);
-              const labels = prediction.label.split(/,\s*/);
-              const boundingBox = prediction.boundingBox;
-              const minX = boundingBox.minX;
-              const minY = boundingBox.minY;
-              const maxX = boundingBox.maxX;
-              const maxY = boundingBox.maxY;
-              let color = '#fff';
-              if (probability < .5) color = '#777';
-              return ( < div onClick = {
-                  this.highlight(minX, minY, maxX, maxY)
+
+      return ( < StaggeredMotion defaultStyles = {
+          contents.map(p => ({
+            maxHeight: 0
+          }))
+        }
+        styles = {
+          prevInterpolatedStyles =>
+          prevInterpolatedStyles.map((_, i) =>
+            i === 0 ? {
+              maxHeight: spring(100)
+            } : {
+              maxHeight: spring(prevInterpolatedStyles[i - 1].maxHeight)
+            }
+          )
+        } > {
+          interpolatingStyles =>
+          <
+          div className = "predictions" > {
+            interpolatingStyles.map((style, i) => {
+                const prediction = contents[i];
+                if (prediction == null) {
+                  return null;
                 }
-                className = 'prediction'
-                key = {
-                  `prediction-${i}`
-                }
-                style = {
-                  Object.assign(style, {
-                    color: color,
-                    backgroundColor: `rgba(0,119,187,${probability})`
-                  })
-                } >
-                <
-                h2 > {
-                  labels[0]
-                }({
-                  minX
-                }, {
-                  minY
-                }), ({
-                  maxX
-                }, {
-                  maxY
-                }) < span className = "probability"
-                title = "Probability" > {
-                  percent
-                } % < /span> < /
-                h2 > {
-                  labels[1] != null ?
+                const probability = prediction.probability;
+                const percent = Math.round(probability * 100);
+                const labels = prediction.label.split(/,\s*/);
+                const boundingBox = prediction.boundingBox;
+                const minX = boundingBox.minX;
+                const minY = boundingBox.minY;
+                const maxX = boundingBox.maxX;
+                const maxY = boundingBox.maxY;
+                let color = '#fff';
+                if (probability < .5) color = '#777';
+                return ( < div onClick = {
+                    () => this.hightlight(minX, minY, maxX, maxY)
+                  }
+                  className = 'prediction'
+                  key = {
+                    `prediction-${i}`
+                  }
+                  style = {
+                    Object.assign(style, {
+                      color: color,
+                      backgroundColor: `rgba(0,119,187,${probability})`
+                    })
+                  } >
                   <
-                  p className = "alt-labels" > {
-                    labels.slice(1, labels.length).join(', ')
-                  } < /p> :
-                  null
-                }
+                  h2 > {
+                    labels[0]
+                  }({
+                    minX
+                  }, {
+                    minY
+                  }), ({
+                    maxX
+                  }, {
+                    maxY
+                  }) < span className = "probability"
+                  title = "Probability" > {
+                    percent
+                  } % < /span></h
+                  2 > {
+                    labels[1] != null ?
+                    <
+                    p className = "alt-labels" > {
+                      labels.slice(1, labels.length).join(', ')
+                    } < /p> :
+                    null
+                  }
 
-                <
-                /div>);
-              })
+                  <
+                  /div>);
+                })
+            } <
+            /div>
           } <
-          /div>
-        } <
-        /StaggeredMotion>
+          /StaggeredMotion>
 
-      );
+        );
+      }
     }
-  }
 
   export default Predictions;
